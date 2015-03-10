12e8c1184c1df83f23fc21e0107141ed6f915610
Diogo Machado <diogo.laginha.machado@gmail.com>
2015-03-10 18:44:38 +0000

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 from setuptools import setup, find_packages
+from pip.req import parse_requirements
 
 install_reqs = parse_requirements("requirements.txt")
 reqs = [str(each.req) for each in install_reqs if each.req]
